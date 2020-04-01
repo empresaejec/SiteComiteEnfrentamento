f5e096ed8038638076a9065fa8f9e7a6532dabb3
nathan-vm <nathan.v.marcelino@gmail.com>
2020-04-01 16:18:51 -0300

    adicionar estilo

--- src/components/Radio/index.jsx
+++ src/components/Radio/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import {Field} from 'formik'
+import "./styles.css"
 
 export default function Radio(props){
   return (
   <Field
     name={props.nameValue}
-    render={({field, form, meta})=>(
-        <div>
+    render={({field})=>(
+        <div className="question"> 
             <label htmlFor="question">{props.question}</label>
           <input
             {...field} 
+            className="option"
             value= "true"
             checked={field.value === "true"} 
             name={props.nameValue}
@@ -17,6 +19,7 @@ export default function Radio(props){
           /> <label htmlFor="yes">{props.option1 ? props.option1 : "Sim"}</label>
           <input
             {...field} 
+            className="option"
             value="false"
             checked={field.value === 'false'} 
             name={props.nameValue}

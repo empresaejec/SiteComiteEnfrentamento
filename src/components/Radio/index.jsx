dd9ba60b8d5651caa0be40d2b862730e2baa4ee1
nathan-vm <nathan.v.marcelino@gmail.com>
2020-04-01 16:09:11 -0300

    perguntas adicionadas

--- src/components/Radio/index.jsx
+++ src/components/Radio/index.jsx
@@ -4,22 +4,22 @@ import {Field} from 'formik'
 export default function Radio(props){
   return (
   <Field
-    name={props.name}
-    render={({field})=>(
+    name={props.nameValue}
+    render={({field, form, meta})=>(
         <div>
             <label htmlFor="question">{props.question}</label>
           <input
             {...field} 
             value= "true"
             checked={field.value === "true"} 
-            name={props.name}
+            name={props.nameValue}
             type="radio" 
           /> <label htmlFor="yes">{props.option1 ? props.option1 : "Sim"}</label>
           <input
             {...field} 
             value="false"
             checked={field.value === 'false'} 
-            name={props.name}
+            name={props.nameValue}
             type="radio" 
           /><label htmlFor="yes">{props.option1 ? props.option2 : "Nao"}</label>
         </div>
--- src/pages/Form/index.jsx
+++ src/pages/Form/index.jsx
@@ -1,32 +1,37 @@
 import React from 'react'
 import './styles.css'
 import {Formik} from 'formik'
-import Radio from '../../components/Radio'
+import Radio from "../../components/Radio/index"
 
 function Form(){
     
     return(
         <Formik 
-            initialValues={{
-                type:'false', 
+            initialValues={{ 
                 cpf:"",     
                 name:"",
                 birthday:"",
                 city:"",
                 uf:"",
-
+                q1:"false",
+                q2:"false",
+                q3:"false",
+                q4:"false",
+                q5:"false",
+                q6:"false",
+                q7:"false",
+                q8:"false",
+                q9:"false",
+                q10:"false",
+                q11:"false",
+                q12:"false",
+                q13:"false",
                 }}
             onSubmit={values => alert(JSON.stringify(values, null, 2))}
         >
         {({values,errors,touched,handleChange,handleBlur}) => (
-            <form className="form">
+            <form>
                 {JSON.stringify(values)}
-
-                <Radio
-                    name = 'type'
-                    question = "tomou vacina?"
-                />
-
                 <div>
                     <input 
                     type="text" 
@@ -81,86 +86,63 @@ function Form(){
                     onBlur={handleBlur}
                     value={values.uf}/>
                 </div>
-            
+                <Radio
+                    className="radio"
+                    nameValue = 'q1'
+                    question = "Você fez a vacina de Influenza (Gripe A) recentemente?"
+                />
+                <Radio
+                    nameValue = 'q2'
+                    question = "Você é hipertenso?"
+                />
+                <Radio
+                    nameValue = 'q3'
+                    question = "Você é diabético? "
+                />
+                <Radio
+                    nameValue = 'q4'
+                    question = "Você tem DPOC, asma ou alguma complicação respiratória?"
+                />
+                <Radio
+                    nameValue = 'q5'
+                    question = "Você possui problemas do coração?"
+                />
+                <Radio
+                    nameValue = 'q6'
+                    question = "Você fez algum transplante?"
+                />
+                <Radio
+                    nameValue = 'q7'
+                    question = "Você possui Câncer e fez quimioterapia nos últimos 30 dias?"
+                />
+                <Radio
+                    nameValue = 'q8'
+                    question = "Você é portador do vírus HIV/Aids?"
+                />
+                <Radio
+                    nameValue = 'q9'
+                    question = "Você está grávida?"
+                />
+                <Radio
+                    nameValue = 'q10'
+                    question = "Você possui problemas nos rins?"
+                />
+                <Radio
+                    nameValue = 'q11'
+                    question = "Você possui algum outro problema de saúde?"
+                />
+                <Radio
+                    nameValue = 'q12'
+                    question = "Você teve contato com alguém que testou positivo para o Coronavírus ou que tenha suspeita de estar infectado com o Coronavírus?"
+                />
+                <Radio
+                    nameValue = 'q13'
+                    question = "Você realizou uma viagem recentemente?"
+                />
             </form>
         )}
 
-            {/* <input type="text" name="cpf" className="form-text" onChange={setCpf} placeholder="CPF"/>
-
-            <input type="text" name="name" className="form-text" onChange={setName} placeholder="Nome"/>
-
-            <input type="datetime" name="birthday" className="form-date" onChange={setBirthday} placeholder="31/03/2020"/>
-
-            <input type="text" name="city" className="form-text" onChange={setCity} placeholder="Cidade"/>
-
-            <input type="text" name="uf" className="form-text" onChange={setUf} placeholder="UF"/>
-
-            <div>Você fez a vacina de Influenza (Gripe A) recentemente?
-                <input type="radio" name="q1" className='form-radio' value="yes"/>Sim
-                <input type="radio" name="q1" className='form-radio' value="no" checked/>Não
-            </div>
-            <span>Você é hipertenso?
-                <input type="radio" name="q2" className='form-radio' value="yes"/>Sim
-                <input type="radio" name="q2" className='form-radio' value="no" checked/>Não
-            </span>
-
-            <span>Você é diabético? 
-                <input type="radio" name="q3" className='form-radio' value="yes"/>Sim
-                <input type="radio" name="q3" className='form-radio' value="no" checked/>Não
-            </span>
-
-            <span>Você tem DPOC, asma ou alguma complicação respiratória?
-                <input type="radio" name="q4" className='form-radio' value="yes"/>Sim
-                <input type="radio" name="q4" className='form-radio' value="no" checked/>Não
-            </span>
-
-            <span>Você possui problemas do coração?
-                <input type="radio" name="q5" className='form-radio' value="yes"/>Sim
-                <input type="radio" name="q5" className='form-radio' value="no" checked/>Não
-            </span>
-
-            <span>Você fez algum transplante?
-                <input type="radio" name="q6" className='form-radio' value="yes"/>Sim
-                <input type="radio" name="q6" className='form-radio' value="no" checked/>Não
-            </span>
-
-            <span>Você possui Câncer e fez quimioterapia nos últimos 30 dias?
-                <input type="radio" name="q7" className='form-radio' value="yes"/>Sim
-                <input type="radio" name="q7" className='form-radio' value="no" checked/>Não
-            </span>
-
-            <span>Você é portador do vírus HIV/Aids?
-                <input type="radio" name="q8" className='form-radio' value="yes"/>Sim
-                <input type="radio" name="q8" className='form-radio' value="no" checked/>Não
-            </span>
-
-            <span>Você está grávida?
-                <input type="radio" name="q9" className='form-radio' value="yes"/>Sim
-                <input type="radio" name="q9" className='form-radio' value="no" checked/>Não
-            </span>
-
-            <span>Você possui problemas nos rins?
-                <input type="radio" name="q10" className='form-radio' value="yes"/>Sim
-                <input type="radio" name="q10" className='form-radio' value="no" checked/>Não
-            </span>
-
-            <span>Você possui algum outro problema de saúde?
-                <input type="radio" name="q11" className='form-radio' value="yes"/>Sim
-                <input type="radio" name="q11" className='form-radio' value="no" checked/>Não
-                <input type='text' name="q11-description" className='form-text'/>
-            </span>
-
-
-            <span>Você teve contato com alguém que testou positivo para o Coronavírus ou que tenha suspeita de estar infectado com o Coronavírus?
-                <input type="radio" name="q12" className='form-radio' value="yes"/>Sim
-                <input type="radio" name="q12" className='form-radio' value="no" checked/>Não
-            </span>
-
-            <span>Você realizou uma viagem recentemente?
-                <input type="radio" name="q13" className='form-radio' value="yes"/>Sim
-                <input type="radio" name="q13" className='form-radio' value="no" checked/>Não
-            </span>
-            <span>Quais desses sintomas você apresenta?
+            {/* <span>Quais desses sintomas você apresenta?
                 <input type="checkbox" name="q14a" />Dificuldade para respirar
                 <input type="checkbox" name="q14b" /> Febre maior ou igual a 38ºC
                 <input type="checkbox" name="q14c" /> Dores no corpo
